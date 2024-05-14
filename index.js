a027eb2fd979f5d49345ba8292c9da82d5bf86e2
Shubham Singh <133738315+shubham72-73@users.noreply.github.com>
2024-05-14 10:09:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 
 // CORS configuration
 app.use((req,res,next)=>{
-    res.setHeader("Access-Control-Allow-Origin","http://localhost:3000");
+    res.setHeader("Access-Control-Allow-Origin","https://khaayo.netlify.app");
     res.header(
         "Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept" 
